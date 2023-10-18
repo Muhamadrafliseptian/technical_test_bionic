b00178876d1a682236c5dd58c03957f4207989ff
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2023-10-18 20:19:43 +0700

    Field

--- src/components/partials-components/InputField.vue
+++ src/components/partials-components/InputField.vue
@@ -1,8 +1,12 @@
 <template>
-    <input class="form-control form-control-sm" :type="Tipe" :placeholder="placeholder" aria-label=".form-control-sm example">
+    <Field class="form-control form-control-sm" :type="Tipe" :placeholder="placeholder" aria-label=".form-control-sm example"/>
 </template>
 <script>
+import {Field} from 'vee-validate'
 export default {
+    components: {
+        Field
+    },
     props: {
         Tipe:{
             type: String,
